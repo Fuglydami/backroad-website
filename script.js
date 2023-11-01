423c3201aafc7b189739efc3ac091dfe79ff8507
Damilare Oyedeji <damoye81@gmail.com>
2023-11-01 12:17:39 +0100

    new update

---
+++ script.js
@@ -0,0 +1,72 @@
+//console.log('my name is' + ' ' + 'Dami');
+
+// var
+// let
+// const
+
+// data types
+// variable
+// strings
+// bolean
+// integers
+
+// null
+// undefined
+// objects
+// equalities
+
+// if and else block
+
+let text = 'my name is ';
+let my_name = 'dami';
+let dami = 208024352304878932;
+let ayo = 823892345892348;
+let chukuma = 76237834;
+
+console.log(dami - ayo - chukuma);
+
+// camel case
+const iLovePizza = true;
+// UpperCamelCase
+const ILoveToEatHotDogs = false;
+// snake case
+const this_is_snake_case = 'cool';
+
+const sentence = `she's so cool`;
+// console.log(1 + '2');
+// gender = 'female';
+
+const name = 'dami';
+const hello = `hello name is ${name}, Nice to meet you. I am ${
+  1 + 5
+} years old`;
+
+let checkAge = true;
+
+if (checkAge === true) {
+  console.log('correct');
+} else {
+  console.log('wrong');
+}
+
+// console.log(hello);
+
+// console.log(text + myName);
+
+// template literal
+
+// console.log(`my name is ${myName} and i am ${age} years old`);
+// console.log(gender);
+// console.log(checkAge);
+
+// code block
+
+// 1)age is less than 18
+// or
+//  2) age is equal to 18
+
+// if (dami <= ayo) {
+//   console.log('Dami is ayos age mates');
+// } else {
+//   console.log('Dami is the younger brother to ayo');
+// }
